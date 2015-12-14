1b23483505d973de00bbce05598e7edc45078ec0
ex0804992 <ex200208049@gmail.com>
2015-12-15 02:06:51 +0800

    Add some test code
    Add some test code

---
+++ src/test/java/TCPServerTest.java
@@ -0,0 +1,36 @@
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
+
+public class TCPServerTest extends TestCase {
+
+    public TCPServerTest(){
+
+
+
+    }
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testInitTCPServer() throws Exception {
+        fail("not yet");
+    }
+
+    @Test
+    public void testGetClientIPTable() throws Exception {
+        fail("not yet");
+    }
+}
